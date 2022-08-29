50152afe165c8c10e7debcb4322938751353376c
miltondw <estradamilton2001@gmail.com>
2022-08-29 17:19:14 -0500

    mutation like post

--- graphql/resolvers/Posts.js
+++ graphql/resolvers/Posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server")
+const { AuthenticationError, UserInputError } = require("apollo-server")
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth")
 module.exports = {
@@ -45,6 +45,22 @@ module.exports = {
       } catch (error) {
         throw new Error(error)
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context)
+      const post = await Post.findById(postId)
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          post.likes = post.likes.filter(like => like.username !== username)
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          })
+        }
+        await post.save()
+        return post
+      } else throw new UserInputError('Post not found')
     }
   }
 };
