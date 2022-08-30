91f97f0f0ee8494cc06f5edfd895ec1df737c318
miltondw <estradamilton2001@gmail.com>
2022-08-30 11:26:31 -0500

    configuration of the router and apolloProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apollo/client'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Atom/Navbar';
+import './global.css'
+import Home from './components/Pages/Home';
+import Login from './components/Pages/Login';
+import Register from './components/Pages/Register';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+
+        </Routes>
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
