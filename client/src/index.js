f72debf253b1e80242c0abe9d741e6b6fb40f82b
miltondw <estradamilton2001@gmail.com>
2022-08-31 13:59:17 -0500

    Context

--- client/src/index.js
+++ client/src/index.js
@@ -12,20 +12,22 @@ import './global.css'
 import Home from './components/Pages/Home';
 import Login from './components/Pages/Login';
 import Register from './components/Pages/Register';
-
+import { AuthProvider } from './context/auth'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </Router>
-    </ApolloProvider>
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </Router>
+      </ApolloProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
