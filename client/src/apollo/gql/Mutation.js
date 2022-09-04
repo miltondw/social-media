a40e96397cbd36f52a6adbf009b6f66de08ddf2d
miltondw <estradamilton2001@gmail.com>
2022-09-03 19:19:31 -0500

    create comments and posts

--- client/src/apollo/gql/Mutation.js
+++ client/src/apollo/gql/Mutation.js
@@ -55,6 +55,15 @@ const CREATE_POST = gql`
     }
   }
 `;
+const CREATE_COMMENT = gql`
+  mutation createComment($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      body
+      commentCount
+      username
+    }
+  }
+`;
 const LIKE_POST = gql`
   mutation likePost($postId: ID!) {
     likePost(postId: $postId) {
@@ -66,5 +75,28 @@ const LIKE_POST = gql`
     }
   }
 `;
-
-export { REGISTER, LOGIN, CREATE_POST, LIKE_POST };
+const DELETE_POST = gql`
+  mutation DeletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+const DELETE_COMMENT = gql`
+  mutation DeleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+      }
+    }
+  }
+`;
+export {
+  REGISTER,
+  LOGIN,
+  CREATE_POST,
+  LIKE_POST,
+  DELETE_POST,
+  DELETE_COMMENT,
+  CREATE_COMMENT,
+};
